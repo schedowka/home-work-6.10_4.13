84341f6c0bff2486b38be8b3803a94e5a83c39af
schedowka <marina_molodkina@proton.me>
2024-10-08 20:50:52 +0300

    4.13

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        System.out.println(" " +a +b +c);
+        double d = (b*b) - 4*a*c;
+        if (d<0) {
+            System.out.println("вещественных корней нет");
+        }
+        else {
+            System.out.println("вещественные корни есть");
+        }
+
+
+    }
+}
